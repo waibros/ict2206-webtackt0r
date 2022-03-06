e022df48c3c890475630fcbf9f9c4ec4c294690c
Yiu Wai Leong <waileong.yiu@gmail.com>
2022-03-06 14:41:15 +0800

    added more regex to detect encoded sql injection

--- additional_scripts/encoding_regex.py
+++ additional_scripts/encoding_regex.py
@@ -4,7 +4,7 @@ import urllib.parse
 This script is to double-encode SQL keywords and format into regular expression form. 
 '''
 keywords = ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'COLLATE', 'CASE', 'SUBSTR', 'SUBSTRING', 'NULL', 'ORDER BY', 'UNION', 'UNION ALL', 'FROM dual']
-final_text = ''
+single_encoded = double_encoded = unicode_encoded = charunicodeescape = ''
 for keyword in keywords:
     temp = ''
     for character in keyword:
@@ -12,6 +12,13 @@ for keyword in keywords:
             temp += "%" + format(ord(character), "x")
         else:
             temp += urllib.parse.quote(character, safe='')
-    final_text += urllib.parse.quote(temp, safe='').upper() + "|"
 
-print(final_text)
+    single_encoded += temp.upper() + "|"
+    double_encoded += urllib.parse.quote(temp, safe='').upper() + "|"
+    unicode_encoded += temp.upper().replace("%", "%u00") + "|"
+    charunicodeescape += temp.upper().replace("%", "%5Cu00") + "|"
+
+print("Single encoded regex: \n" + single_encoded)
+print("\nDouble encoded regex: \n" + double_encoded)
+print("\nUnicode encoded regex: \n" + unicode_encoded)
+print("\nCharunicodeescape tamper script regex: \n" + charunicodeescape)
